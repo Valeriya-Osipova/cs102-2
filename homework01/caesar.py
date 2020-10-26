e95fff8bb415353c1fe9b707abadaab6f88ca35b
valeriya <64967406+Valeriya-Osipova@users.noreply.github.com>
2020-10-26 14:49:51 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,6 @@
 import typing as tp
-
-
+m = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -15,7 +15,15 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if i in m:
+            index = m.index(i)
+            ciphertext += m[(index+shift)%len(m)]
+        elif i in b:
+            index = b.index(i)
+            ciphertext += b[(index+shift)%len(b)]
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -33,7 +41,15 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if i in m:
+            index = m.index(i)
+            plaintext += m[(index-shift)%len(m)]
+        elif i in b:
+            index = b.index(i)
+            plaintext += b[(index- shift)%len(b)]
+        else:
+            plaintext += i
     return plaintext
 
 
