4797f6ebb627ef64795251e90eb1cd3daa037636
valeriya <64967406+Valeriya-Osipova@users.noreply.github.com>
2020-10-26 14:39:41 +0300

    Update caesar.go

--- homework01-go/caesar/caesar.go
+++ homework01-go/caesar/caesar.go
@@ -1,9 +1,19 @@
 package caesar
-
+m = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 func EncryptCaesar(plaintext string, shift int) string {
 	var ciphertext string
 
-	// PUT YOUR CODE HERE
+	ciphertext = ""
+    for i in plaintext:
+        if i in m:
+            index = m.index(i)
+            ciphertext += m[(index+shift)%len(m)]
+        elif i in b:
+            index = b.index(i)
+            ciphertext += b[(index+shift)%len(b)]
+        else:
+            ciphertext += i
 
 	return ciphertext
 }
@@ -11,7 +21,19 @@ func EncryptCaesar(plaintext string, shift int) string {
 func DecryptCaesar(ciphertext string, shift int) string {
 	var plaintext string
 
-	// PUT YOUR CODE HERE
+    plaintext = ""
+    for i in ciphertext:
+        if i in m:
+            index = m.index(i)
+            plaintext += m[(index-shift)%len(m)]
+        elif i in b:
+            index = b.index(i)
+            plaintext += b[(index- shift)%len(b)]
+        else:
+            plaintext += i
+    return plaintext
 
 	return plaintext
 }
+print(encrypt_caesar(input()))
+print(decrypt_caesar(input()))
