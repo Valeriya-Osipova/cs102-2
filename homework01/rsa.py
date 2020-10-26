38a5354a69a505138b4eceba647ff3344500a721
valeriya <64967406+Valeriya-Osipova@users.noreply.github.com>
2020-10-26 14:54:15 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,7 +13,21 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    from math import ceil
+    a=n
+    b=0
+    for i in range(2,ceil(a**0.5)+1):
+        if a%i==0:
+            b+=1
+            break
+    if a==1 :
+        return bool(0) 
+    elif a==2:
+        return bool(1)
+    elif b==0:
+        return bool(1)
+    else:
+        return bool(0) 
     pass
 
 
@@ -26,7 +40,12 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
+    while a!=0 and b!=0:
+        if a>b:
+            a=a%b
+        else:
+            b=b%a
+    return a+b 
     pass
 
 
@@ -38,7 +57,16 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
+    i=1
+    if phi==1:
+        return 0
+    else:
+        while True:
+            if i%e==0:
+                break
+            else:
+                i+=phi
+        return (i//e)
     pass
 
 
@@ -47,29 +75,15 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
-
-    # n = pq
-    # PUT YOUR CODE HERE
-
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
-
-    # Choose an integer e such that e and phi(n) are coprime
+    n=p*q
+    phi = (p-1)*(q-1)
     e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
-    while g != 1:
+    while g != 1:   
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
-
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
-
 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     # Unpack the key into it's components
